a0f319c6acd9ab609585c98e878e5c08f233ac84
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2020-09-01 17:07:51 -0300

    fix erros on tests

--- test/utils/json_isolate_test.dart
+++ test/utils/json_isolate_test.dart
@@ -3,21 +3,21 @@ import 'package:flutter_tools/flutter_tools.dart';
 
 void main() {
   test('Should encode json', () async {
-    final encoded = await encodeJson({"teste": "teste"});
+    final encoded = await encodeToJson({"teste": "teste"});
     expect(encoded, '{"teste":"teste"}');
   });
   test('Should decode json', () async {
-    final decoded = await decodeJson('{"teste":"teste"}');
+    final decoded = await decodeFromJson('{"teste":"teste"}');
     expect(decoded, isA<Map>());
     expect((decoded as Map).containsKey('teste'), isTrue);
     expect(decoded['teste'], 'teste');
   });
   test('Should throw exception with wrong format', () {
     expect(() async {
-      return await decodeJson('');
+      return await decodeFromJson('');
     }, throwsException);
     expect(() async {
-      await encodeJson(Object());
+      await encodeToJson(Object());
     }, throwsException);
   });
 }
