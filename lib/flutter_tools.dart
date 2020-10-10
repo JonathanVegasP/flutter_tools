c214403d8d718e1c4dc81dcc5d26e8f0ea562b2b
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2020-10-10 01:13:27 -0300

    refactoring the package

--- lib/flutter_tools.dart
+++ lib/flutter_tools.dart
@@ -1,9 +1,1 @@
 library flutter_tools;
-
-export 'src/injection/injection_impl.dart';
-export 'src/observable/observable_impl.dart';
-export 'src/observers/observer.dart';
-export 'src/observers/observer_value.dart';
-export 'src/router/router_impl.dart';
-export 'src/utils/json_isolate.dart';
-export 'src/utils/validators.dart';
